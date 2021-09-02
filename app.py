e6cd94b239f5c8f0282b252964f93ae8dfe3a892
Gabriel Amorim <gabriel.2019206500@unicap.br>
2021-09-02 13:12:04 -0300

    Update with Routes

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask
 import requests
-
+import datetime
 
 app = Flask(__name__)
 
-#URL Mockada para consultas é só alterar
 url = "https://612db2b8e579e1001791dc8d.mockapi.io/api/v1/users"
 
-x = requests.get(url)
+get_result = requests.get(url)
 
-@app.route("/")
+@app.route("/usuarios")
+def pegar_usuarios():
+    get_json = get_result.json()
+    users = []
 
-def hello_world():
-    return x.text
+    for i in get_json:
+        users.append(i['name'])
 
+    return str(users)
+
+@app.route("/criado")
+def pegar_datas_criacao():
+    get_json = get_result.json()
+
+    created = []
+
+    for i in get_json:
+        created.append(i['createdAt'])
+
+    aux = datetime.datetime(created)
+    return aux.strftime('%m/%d/%Y')
